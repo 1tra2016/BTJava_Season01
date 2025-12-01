08b6d4558fdc40067230b807c0e1438b9ee79a34
Chè <119411856+1tra2016@users.noreply.github.com>
2025-12-01 21:27:44 +0700

    Add files via upload

---
+++ kha02/out/production/new/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Nhập số thứ nhất:");
+        int firstNumber = scan.nextInt();
+        System.out.println("Nhập số thứ hai:");
+        int secondNumber = scan.nextInt();
+
+        System.out.println("---Kết quả---");
+        System.out.printf("firstNumber =  %d\n", firstNumber);
+        System.out.printf("secondNumber =  %d\n", secondNumber);
+        System.out.printf("Tổng =  %d\n", firstNumber+secondNumber);
+        System.out.printf("Hiệu =  %d\n", firstNumber-secondNumber);
+        System.out.printf("Tích =  %d\n", firstNumber*secondNumber);
+        System.out.printf("Thương =  %d\n", firstNumber/secondNumber);
+        System.out.printf("Phần dư =  %d\n", firstNumber%secondNumber);
+
+    }
+}
